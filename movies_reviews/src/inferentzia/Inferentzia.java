4af934adab881f3c73c9f5c6679c0c8c906f4575
Zihara <araizaloidab@gmail.com>
2017-04-09 19:34:49 +0200

    inf zuz

--- movies_reviews/src/inferentzia/Inferentzia.java
+++ movies_reviews/src/inferentzia/Inferentzia.java
@@ -13,6 +13,8 @@ import weka.filters.Filter;
 import weka.filters.unsupervised.instance.RemovePercentage;
 
 public class Inferentzia {
+	
+	private String txtan = "";
 
 	public static void inferentzia(Instances train, Instances dev, Instances traindev, int clas){
 		RandomForest params = ParametroEkorketa.parametroEkorketa(train, dev, clas);
@@ -47,7 +49,6 @@ public class Inferentzia {
 	
 	private static void ebaluatuNB(Instances train, Instances dev, Instances traindev, int clas) {
 		try {
-			System.out.println("Klase minoritarioa: "+ clas);
 			NaiveBayes nb = new NaiveBayes();
 			System.out.println("NaiveBayes Ez-Zintzoa:");
 			ezzintzoa(nb, traindev, clas);
@@ -92,17 +93,17 @@ public class Inferentzia {
 			System.out.println("Klase minoritarioa: "+ clas);
 			RandomForest rf = new RandomForest();
 			rf.setOptions(cl.getOptions());
-			System.out.println("RandomForest Ez Zintzoa:");
+			System.out.println("\nRandomForest Ez Zintzoa:");
 			ezzintzoa(rf, traindev, clas);
 			rf = new RandomForest();
 			rf.setOptions(cl.getOptions());
-			System.out.println("RandomForest 10 Cross-Validation:");
+			System.out.println("\nRandomForest 10 Cross-Validation:");
 			tencross(rf, traindev, clas);
-			System.out.println("RandomForest Hold Out:");
+			System.out.println("\nRandomForest Hold Out:");
 			holdout(traindev, clas, cl.getOptions());
 			rf = new RandomForest();
 			rf.setOptions(cl.getOptions());
-			System.out.println("NaiveBayes Hold Out (Train vs. Dev)");
+			System.out.println("\nRandomForest Hold Out (Train vs. Dev)");
 			holdouttvsd(rf, train, dev, clas);
 			 
 		} catch (Exception e) {
@@ -113,18 +114,10 @@ public class Inferentzia {
 	
 	private static void holdouttvsd(Classifier cl, Instances train, Instances dev, int clas){
 		try{
-			ArrayList<Double> fm0 = new ArrayList<Double>();
-			ArrayList<Double> fm1 = new ArrayList<Double>();
-			ArrayList<Double> fmw = new ArrayList<Double>();
 			cl.buildClassifier(train);
-			for(int i = 0; i>100; i++){
-				Evaluation eval= new Evaluation(train);
-				eval.evaluateModel(cl, dev);
-				 fm0.add(eval.fMeasure(0));
-			     fm1.add(eval.fMeasure(1));
-			     fmw.add(eval.weightedFMeasure());
-			}
-			inprimatu2(fm0,fm1,fmw);
+			Evaluation eval= new Evaluation(train);
+			eval.evaluateModel(cl, dev);
+			inprimatu(eval, clas);
 		} catch (Exception e){
 			e.printStackTrace();
 		}
@@ -151,13 +144,18 @@ public class Inferentzia {
 		ArrayList<Double> fm0 = new ArrayList<Double>();
 		ArrayList<Double> fm1 = new ArrayList<Double>();
 		ArrayList<Double> fmw = new ArrayList<Double>();
+		ArrayList<Double> p0 = new ArrayList<Double>();
+		ArrayList<Double> p1 = new ArrayList<Double>();
+		ArrayList<Double> pw = new ArrayList<Double>();
+		ArrayList<Double> r0 = new ArrayList<Double>();
+		ArrayList<Double> r1 = new ArrayList<Double>();
+		ArrayList<Double> rw = new ArrayList<Double>();
 		RandomForest rf;
 		try{
 			for(int i= 1;i<=100; i++ ){
 				//Hold-out
 				rf = new RandomForest();
 				rf.setOptions(opt);
-				System.out.println("Hold out egiten Random("+(i+1)+")...");
 				data.randomize(new Random(i+1));
 				RemovePercentage rp = new RemovePercentage();
 				rp.setInputFormat(data);
@@ -174,8 +172,14 @@ public class Inferentzia {
 		        fm0.add(eval.fMeasure(0));
 		        fm1.add(eval.fMeasure(1));
 		        fmw.add(eval.weightedFMeasure());
+		        p0.add(eval.precision(0));
+		        p1.add(eval.precision(1));
+		        pw.add(eval.weightedPrecision());
+		        r0.add(eval.recall(0));
+		        r1.add(eval.recall(1));
+		        rw.add(eval.weightedRecall());
 			}
-			inprimatu2(fm0,fm1,fmw);
+			inprimatu2(fm0,fm1,fmw,p0,p1,pw,r0,r1,rw);
 			
 		} catch (Exception e){
 			e.printStackTrace();
@@ -186,12 +190,17 @@ public class Inferentzia {
 		ArrayList<Double> fm0 = new ArrayList<Double>();
 		ArrayList<Double> fm1 = new ArrayList<Double>();
 		ArrayList<Double> fmw = new ArrayList<Double>();
+		ArrayList<Double> p0 = new ArrayList<Double>();
+		ArrayList<Double> p1 = new ArrayList<Double>();
+		ArrayList<Double> pw = new ArrayList<Double>();
+		ArrayList<Double> r0 = new ArrayList<Double>();
+		ArrayList<Double> r1 = new ArrayList<Double>();
+		ArrayList<Double> rw = new ArrayList<Double>();
 		NaiveBayes nb;
 		try{
-			for(int i= 1;i<=100; i++ ){
+			for(int i= 1;i<=10; i++ ){
 				//Hold-out
 				nb = new NaiveBayes();
-				System.out.println("Hold out egiten Random("+(i+1)+")...");
 				data.randomize(new Random(i+1));
 				RemovePercentage rp = new RemovePercentage();
 				rp.setInputFormat(data);
@@ -208,30 +217,67 @@ public class Inferentzia {
 		        fm0.add(eval.fMeasure(0));
 		        fm1.add(eval.fMeasure(1));
 		        fmw.add(eval.weightedFMeasure());
+		        p0.add(eval.precision(0));
+		        p1.add(eval.precision(1));
+		        pw.add(eval.weightedPrecision());
+		        r0.add(eval.recall(0));
+		        r1.add(eval.recall(1));
+		        rw.add(eval.weightedRecall());
+		        
 			}
-			inprimatu2(fm0,fm1,fmw);
+			inprimatu2(fm0,fm1,fmw,p0,p1,pw,r0,r1,rw);
 			
 		} catch (Exception e){
 			e.printStackTrace();
 		}
 	}
 	
-	private static void inprimatu2(ArrayList<Double> fm0, ArrayList<Double> fm1, ArrayList<Double> fmw){
-		double media = getMean(fm0);
-		double var = getVariance(fm0, media);
-		double des = getStdDev(var);
-		System.out.println("Fmeasure(0)-ren bataz bestekoa: "+media);
-		System.out.println("Fmeasure(0)-ren desbiderapena: "+ des);
-		media = getMean(fm1);
-		var= getVariance(fm1, media);
-		des = getStdDev(var);
-		System.out.println("Fmeasure(1)-ren bataz bestekoa: "+media);
-		System.out.println("Fmeasure(1)-ren desbiderapena: "+ des);
-		media = getMean(fmw);
-		var= getVariance(fmw, media);
-		des = getStdDev(var);
-		System.out.println("Weighted Fmeasure-ren bataz bestekoa: "+media);
-		System.out.println("WeightedFmeasure-ren desbiderapena: "+ des+"\n");
+	private static void inprimatu2(ArrayList<Double> fm0, ArrayList<Double> fm1, ArrayList<Double> fmw,ArrayList<Double> p0, ArrayList<Double> p1, ArrayList<Double> pw,ArrayList<Double> r0, ArrayList<Double> r1, ArrayList<Double> rw){
+		double fmedia = getMean(fm0);
+		double fvar = getVariance(fm0, fmedia);
+		double fdes = getStdDev(fvar);
+		System.out.println("\nFmeasure(0)-ren bataz bestekoa: "+fmedia);
+		System.out.println("Fmeasure(0)-ren desbiderapena: "+ fdes);
+		fmedia = getMean(fm1);
+		fvar= getVariance(fm1, fmedia);
+		fdes = getStdDev(fvar);
+		System.out.println("Fmeasure(1)-ren bataz bestekoa: "+fmedia);
+		System.out.println("Fmeasure(1)-ren desbiderapena: "+ fdes);
+		fmedia = getMean(fmw);
+		fvar= getVariance(fmw, fmedia);
+		fdes = getStdDev(fvar);
+		System.out.println("Weighted Fmeasure-ren bataz bestekoa: "+fmedia);
+		System.out.println("WeightedFmeasure-ren desbiderapena: "+ fdes+"\n");
+		double pmedia = getMean(p0);
+		double pvar = getVariance(p0, pmedia);
+		double pdes = getStdDev(pvar);
+		System.out.println("\nPrecision(0)-ren bataz bestekoa: "+pmedia);
+		System.out.println("Precision(0)-ren desbiderapena: "+ pdes);
+		pmedia = getMean(p1);
+		pvar= getVariance(p1, pmedia);
+		pdes = getStdDev(pvar);
+		System.out.println("Precision(1)-ren bataz bestekoa: "+pmedia);
+		System.out.println("Precision(1)-ren desbiderapena: "+ pdes);
+		pmedia = getMean(pw);
+		pvar= getVariance(pw, pmedia);
+		pdes = getStdDev(pvar);
+		System.out.println("Weighted Precision-ren bataz bestekoa: "+pmedia);
+		System.out.println("Weighted Precision-ren desbiderapena: "+ pdes+"\n");
+		double rmedia = getMean(r0);
+		double rvar = getVariance(r0, rmedia);
+		double rdes = getStdDev(rvar);
+		System.out.println("\nRecall(0)-ren bataz bestekoa: "+rmedia);
+		System.out.println("Recall(0)-ren desbiderapena: "+ rdes);
+		rmedia = getMean(r1);
+		rvar= getVariance(r1, rmedia);
+		rdes = getStdDev(rvar);
+		System.out.println("Recall(1)-ren bataz bestekoa: "+rmedia);
+		System.out.println("Recall(1)-ren desbiderapena: "+ rdes);
+		rmedia = getMean(rw);
+		rvar= getVariance(rw, rmedia);
+		rdes = getStdDev(rvar);
+		System.out.println("Weighted Recall-ren bataz bestekoa: "+rmedia);
+		System.out.println("Weighted Recall-ren desbiderapena: "+ rdes+"\n");
 	}
 	
 	private static void tencross(Classifier cl, Instances data, int clas) {
@@ -240,16 +286,27 @@ public class Inferentzia {
 			ArrayList<Double> fm0 = new ArrayList<Double>();
 			ArrayList<Double> fm1 = new ArrayList<Double>();
 			ArrayList<Double> fmw = new ArrayList<Double>();
+			ArrayList<Double> p0 = new ArrayList<Double>();
+			ArrayList<Double> p1 = new ArrayList<Double>();
+			ArrayList<Double> pw = new ArrayList<Double>();
+			ArrayList<Double> r0 = new ArrayList<Double>();
+			ArrayList<Double> r1 = new ArrayList<Double>();
+			ArrayList<Double> rw = new ArrayList<Double>();
 			cl.buildClassifier(data);
-			for (int i = 1; i<=100; i++ ){
+			for (int i = 1; i<=10; i++ ){
 				eval = new Evaluation(data);
-				System.out.println("10 fold cross validation egiten Random("+(i+1)+")...");
 				eval.crossValidateModel(cl, data, 10, new Random(i+1));
 				fm0.add(eval.fMeasure(0));
 		        fm1.add(eval.fMeasure(1));
 		        fmw.add(eval.weightedFMeasure());
+		        p0.add(eval.precision(0));
+		        p1.add(eval.precision(1));
+		        pw.add(eval.weightedPrecision());
+		        r0.add(eval.recall(0));
+		        r1.add(eval.recall(1));
+		        rw.add(eval.weightedRecall());
 			}
-			inprimatu2(fm0,fm1,fmw);
+			inprimatu2(fm0,fm1,fmw,p0,p1,pw,r0,r1,rw);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -273,10 +330,15 @@ public class Inferentzia {
 	}
 
 	private static void inprimatu(Evaluation eval, int clas) {
-		System.out.println("F-measure: " +eval.fMeasure(clas));
-		System.out.println("Precision: "+ eval.precision(clas));
-		System.out.println("Recall: " +eval.recall(clas));
-		System.out.println("Acc: "+eval.pctCorrect()+"\n");
+		System.out.println("F-measure0: " +eval.fMeasure(0));
+		System.out.println("Precision0: "+ eval.precision(0));
+		System.out.println("Recall0: " +eval.recall(0));
+		System.out.println("\nF-measure1: " +eval.fMeasure(1));
+		System.out.println("Precision1: "+ eval.precision(1));
+		System.out.println("Recall1: " +eval.recall(1));
+		System.out.println("\nF-measurew: " +eval.weightedFMeasure());
+		System.out.println("Precisionw: "+ eval.weightedPrecision());
+		System.out.println("Recallw: " +eval.weightedRecall());
 		
 	}
 	public static void main(String[] args) {
